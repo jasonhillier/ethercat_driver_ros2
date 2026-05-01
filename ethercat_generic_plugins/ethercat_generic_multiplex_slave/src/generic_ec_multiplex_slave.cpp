38f45390b1ee8b6645d263ac8d3de550d42c85a2
jasonhillier <jason@hillier.us>
2026-05-01 16:39:22 -0600

    Update generic_ec_multiplex_slave.cpp

--- ethercat_generic_plugins/ethercat_generic_multiplex_slave/src/generic_ec_multiplex_slave.cpp
+++ ethercat_generic_plugins/ethercat_generic_multiplex_slave/src/generic_ec_multiplex_slave.cpp
@@ -66,11 +66,11 @@ namespace ethercat_generic_plugins
   void EcMultiplexSlave::processData(size_t entry_idx, uint8_t *domain_address)
   {
     // hack copy internal config data !!!
-    alias_ = subUnits_[0]->alias_;
-    position_ = subUnits_[0]->position_;
-    vendor_id_ = subUnits_[0]->vendor_id_;
-    product_id_ = subUnits_[0]->product_id_;
-    sdo_config = subUnits_[0]->sdo_config;
+    // alias_ = subUnits_[0]->alias_;
+    // position_ = subUnits_[0]->position_;
+    // vendor_id_ = subUnits_[0]->vendor_id_;
+    // product_id_ = subUnits_[0]->product_id_;
+    // sdo_config = subUnits_[0]->sdo_config;
     // !!!
 
     subUnits_[0]->processData(entry_idx, domain_address);
@@ -88,6 +88,13 @@ namespace ethercat_generic_plugins
     {
       subUnits_[0] = new EcCiA402Drive();
       auto r = subUnits_[0]->setupSlave(slave_parameters, state_interface, command_interface);
+      // hack copy internal config data !!!
+      alias_ = subUnits_[0]->alias_;
+      position_ = subUnits_[0]->position_;
+      vendor_id_ = subUnits_[0]->vendor_id_;
+      product_id_ = subUnits_[0]->product_id_;
+      sdo_config = subUnits_[0]->sdo_config;
+      // !!!
       return r;
     }
     return true;
