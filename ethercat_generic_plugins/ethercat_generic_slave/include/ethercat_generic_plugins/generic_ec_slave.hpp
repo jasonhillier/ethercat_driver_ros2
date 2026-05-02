a88c86514b7bbb0f744bb402415e741fdb983421
jasonhillier <jason@hillier.us>
2026-05-01 20:14:20 -0600

    Update generic_ec_slave.hpp

--- ethercat_generic_plugins/ethercat_generic_slave/include/ethercat_generic_plugins/generic_ec_slave.hpp
+++ ethercat_generic_plugins/ethercat_generic_slave/include/ethercat_generic_plugins/generic_ec_slave.hpp
@@ -29,47 +29,49 @@
 namespace ethercat_generic_plugins
 {
 
-class GenericEcSlave : public ethercat_interface::EcSlave
-{
-public:
-  GenericEcSlave();
-  virtual ~GenericEcSlave();
-  virtual int assign_activate_dc_sync();
+  class GenericEcSlave : public ethercat_interface::EcSlave
+  {
+  public:
+    GenericEcSlave();
+    virtual ~GenericEcSlave();
+    virtual int assign_activate_dc_sync();
 
-  virtual void processData(size_t entry_idx, uint8_t * domain_address);
+    virtual void processData(size_t entry_idx, uint8_t *domain_address);
 
-  virtual const ec_sync_info_t * syncs();
-  virtual size_t syncSize();
-  virtual const ec_pdo_entry_info_t * channels();
-  virtual void domains(DomainMap & domains) const;
+    virtual const ec_sync_info_t *syncs();
+    virtual size_t syncSize();
+    virtual const ec_pdo_entry_info_t *channels();
+    virtual void domains(DomainMap &domains) const;
 
-  virtual bool setupSlave(
-    std::unordered_map<std::string, std::string> slave_parameters,
-    std::vector<double> * state_interface,
-    std::vector<double> * command_interface);
+    virtual bool setupSlave(
+        std::unordered_map<std::string, std::string> slave_parameters,
+        std::vector<double> *state_interface,
+        std::vector<double> *command_interface);
 
-protected:
-  uint32_t counter_ = 0;
-  std::vector<ec_pdo_info_t> rpdos_;
-  std::vector<ec_pdo_info_t> tpdos_;
-  std::vector<bool> all_channels_skip_list_;
-  std::vector<ec_pdo_entry_info_t> all_channels_;
-  std::vector<ethercat_interface::EcPdoChannelManager *> pdo_channels_info_;
-  std::vector<ethercat_interface::SMConfig> sm_configs_;
-  std::vector<ec_sync_info_t> syncs_;
-  std::vector<unsigned int> domain_map_;
-  YAML::Node slave_config_;
-  uint32_t assign_activate_ = 0;
+    std::vector<ec_pdo_info_t> rpdos_;
+    std::vector<ec_pdo_info_t> tpdos_;
 
-  /** set up of the drive configuration from yaml node*/
-  bool setup_from_config(YAML::Node slave_config);
-  /** set up of the drive configuration from yaml file*/
-  bool setup_from_config_file(std::string config_file);
+  protected:
+    uint32_t counter_ = 0;
 
-  void setup_syncs();
+    std::vector<bool> all_channels_skip_list_;
+    std::vector<ec_pdo_entry_info_t> all_channels_;
+    std::vector<ethercat_interface::EcPdoChannelManager *> pdo_channels_info_;
+    std::vector<ethercat_interface::SMConfig> sm_configs_;
+    std::vector<ec_sync_info_t> syncs_;
+    std::vector<unsigned int> domain_map_;
+    YAML::Node slave_config_;
+    uint32_t assign_activate_ = 0;
 
-  void setup_interface_mapping();
-};
-}  // namespace ethercat_generic_plugins
+    /** set up of the drive configuration from yaml node*/
+    bool setup_from_config(YAML::Node slave_config);
+    /** set up of the drive configuration from yaml file*/
+    bool setup_from_config_file(std::string config_file);
 
-#endif  // ETHERCAT_GENERIC_PLUGINS__GENERIC_EC_SLAVE_HPP_
+    void setup_syncs();
+
+    void setup_interface_mapping();
+  };
+} // namespace ethercat_generic_plugins
+
+#endif // ETHERCAT_GENERIC_PLUGINS__GENERIC_EC_SLAVE_HPP_
